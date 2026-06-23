c8aea5395c8b91a3b07cc069a49572fe87ee1fda
vladislav <vladislavfadin@gmail.com>
2026-06-24 01:15:13 +0300

    add test slices and print

--- main.go
+++ main.go
@@ -2,8 +2,34 @@ package main
 
 import (
 	"fmt"
+
+	"github.com/Vortexcrab/cs-stats-parser/internal/stats"
 )
 
 func main() {
 	fmt.Println("CS Stats Parser starting...")
+
+	match := stats.Match{
+		Nick:        "s1mple",
+		Kills:       10,
+		Deaths:      5,
+		RoundResult: true,
+	}
+	match2 := stats.Match{
+		Nick:        "ZywOo",
+		Kills:       8,
+		Deaths:      7,
+		RoundResult: false,
+	}
+	match3 := stats.Match{
+		Nick:        "device",
+		Kills:       12,
+		Deaths:      4,
+		RoundResult: true,
+	}
+
+	for _, m := range []stats.Match{match, match2, match3} {
+		fmt.Printf("%s kills: %d, deaths: %d, won: %v\n", m.Nick, m.Kills, m.Deaths, m.RoundResult)
+	}
+
 }
